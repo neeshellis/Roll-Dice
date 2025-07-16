cacc1e2f4c92604b2006ec04315bab5dc3c7dc8a
neeshellis <neeshellis@gmail.com>
2025-07-16 18:10:55 +1200

    test update Lab8

--- rolldice.js
+++ rolldice.js
@@ -7,14 +7,14 @@ console.log(getRandomInt(6));
 
 //test
 
-console.log(getRandomInt(7)>7)
+console.log(getRandomInt(7)>7)  //returns false
 
-console.log(getRandomInt(8)>7)
+console.log(getRandomInt(9)>7)  //returns true
 
 
-// if (getRandomInt(7)>0) {
+// if (getRandomInt(7)>0) {      //returns test failed
 
 //   throw new Error("Test failed");
-// }
+//  }
 
 
