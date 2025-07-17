7c42c03fc60ffbb52cd395ecc3d6d1bf2b33c78b
neeshellis <neeshellis@gmail.com>
2025-07-18 10:25:46 +1200

    update Lab8

--- rolldice.js
+++ rolldice.js
@@ -1,20 +1,31 @@
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max) + 1;
+function getRandomInt6(max) {
+const randomNumber=Math.floor(Math.random() * max) + 1;
+document.getElementById("demo6").innerText= randomNumber;
+  return randomNumber
 }
 
-console.log(getRandomInt(6));
+function getRandomInt10(max) {
+const randomNumber=Math.floor(Math.random() * max) + 1;
+document.getElementById("demo10").innerText= randomNumber;
+  return randomNumber
+}
 
+// console.log(getRandomInt(6));
 
 //test
 
-console.log(getRandomInt(7)>7)  //returns false
+//console.log(getRandomInt(7)>7)  //returns false
 
-console.log(getRandomInt(9)>7)  //returns true
+//console.log(getRandomInt(9)>7)  //returns true
 
 
-// if (getRandomInt(7)>0) {      //returns test failed
+// if (getRandomInt6(6)>0) {      //returns test pass
 
 //   throw new Error("Test failed");
 //  }
 
+// if (getRandomInt6(6)>6) {      //returns test failed
+
+//   throw new Error("Test failed");
+//  }
 
